452ed29f087e505de8e1882caebcfd1652922a66
ahmadfikrulumam <umamfa191@gmail.com>
2019-10-03 16:20:59 +0700

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.frontend.master')
+@section('content')
+  <!-- Portfolio Section -->
+  <section id="portfolio">
+    <div class="container-fluid p-0">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center mt-0">
+            <i class="far fa-5x fa-images text-dark mt-5 mb-2"></i>
+            <h2>Album Foto</h2>
+          </div>
+        </div>
+
+        <div class="row no-gutters">
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/1.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/4.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="project-category text-white-50">
+                  Upacara Hari Senin
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/2.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/11.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="project-category text-white-50">
+                  Gerbang Masuk Sekolah
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="project-category text-white-50">
+                  Halaman Sekolah
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/4.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="project-category text-white-50">
+                  Ruang LAB RPL
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/5.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/7.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="project-category text-white-50">
+                  Bisnis Online
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/6.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="">
+              <div class="portfolio-box-caption p-3">
+                <div class="project-category text-white-50">
+                  Kegiatan Pramuka
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+@stop
